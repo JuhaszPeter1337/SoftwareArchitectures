4e7d546c7c41677fb162339582547e0afbe124b1
Juhasz Peter <juhasz.peter1337@gmail.com>
2020-11-25 13:08:58 +0100

    Added checkbox disable
    Signed-off-by: Juhasz Peter <juhasz.peter1337@gmail.com>

--- frontend/js/add.js
+++ frontend/js/add.js
@@ -1,15 +1,36 @@
+function ckChange(ckType){
+    var ckName = document.getElementsByName(ckType.name);
+    var checked = document.getElementById(ckType.id);
+
+    if (checked.checked) {
+      for(var i=0; i < ckName.length; i++){
+
+          if(!ckName[i].checked){
+              ckName[i].disabled = true;
+          }else{
+              ckName[i].disabled = false;
+          }
+      } 
+    }
+    else {
+      for(var i=0; i < ckName.length; i++){
+        ckName[i].disabled = false;
+      } 
+    }    
+}
+
 async function sendEvent(){
     var title = document.getElementById("titlei").value;
     var begin = document.getElementById("begini").value;
     var end = document.getElementById("endi").value;
     var description = document.getElementById("desi").value;
 
-    var interests1 = document.getElementById("i1").checked;
-    var interests2 = document.getElementById("i2").checked;
-    var interests3 = document.getElementById("i3").checked;
-    var interests4 = document.getElementById("i4").checked;
-    var interests5 = document.getElementById("i5").checked;
-    var interests6 = document.getElementById("i6").checked;
+    var interests1 = document.getElementById("progress1").checked;
+    var interests2 = document.getElementById("progress2").checked;
+    var interests3 = document.getElementById("progress3").checked;
+    var interests4 = document.getElementById("progress4").checked;
+    var interests5 = document.getElementById("progress5").checked;
+    var interests6 = document.getElementById("progress6").checked;
 
     var language1 = document.getElementById("l1").checked;
     var language2 = document.getElementById("l2").checked;
@@ -28,10 +49,6 @@ async function sendEvent(){
     event.setInterests(interests1, interests2, interests3, interests4, interests5, interests6);
     event.setLanguages(language1, language2, language3, language4, language5, language6);
 
-    try {
-        await connection.invoke("AddEvent", event);
-      } catch (err) {
-        console.error(err);
-    }
+    console.log(event);
 }
 
